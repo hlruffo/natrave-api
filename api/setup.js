a93a3b1f6a1e816956c042f96927f19a7c109e84
hlruffo <hlruffo@gmail.com>
2022-10-13 10:18:32 -0300

    fetch : setup change

--- api/setup.js
+++ api/setup.js
@@ -5,7 +5,37 @@ import { router } from './router.js'
 
 export const app = new Koa()
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://natrave-web-two-livid.vercel.app/', // front end origin
+    credentials: true // Allow cross domain band cookie
+}))
 app.use(bodyParser())
 app.use(router.routes())
 app.use(router.allowedMethods());
+
+
+
+/* const allowCors = fn => async (ctx) => {
+    ctx.response.setHeader('Access-Control-Allow-Credentials', true)
+    ctx.response.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    ctx.response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    ctx.response.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (ct.method === 'OPTIONS') {
+        ctx.response.status(200).end()
+        return
+    }
+    return await fn(req, res)
+}
+
+const handler = (req, res) => {
+    const d = new Date()
+    res.end(d.toString())
+}
+
+module.exports = allowCors(handler)
+ */
